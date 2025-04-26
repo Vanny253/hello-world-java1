91cb0145ac44f80bcc1ac9168dfb35398c28efbd
Vanny253 <vannylai6011@gmail.com>
2025-04-26 14:53:55 +0800

    Testing 123456

--- bin/main/com/example/App.java
+++ bin/main/com/example/App.java
@@ -1,19 +1,6 @@
-package com.example;
-import static spark.Spark.get;
-import static spark.Spark.port;
-
 public class App {
-
-    private static final String MESSAGE = "Testing 123d456";
-
-    public App() {}
-
     public static void main(String[] args) {
-        port(8081);  // Ensures the app is listening on port 8081
-        get("/", (req, res) -> MESSAGE);
-    }
-
-    public String getMessage() {
-        return MESSAGE;
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
     }
 }
